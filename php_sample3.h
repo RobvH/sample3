38f87940b1280395a0bd636a2cd2ad9c602dfcb1
Rob van Hoose <robertbvanhoose@gmail.com>
2016-05-20 00:23:55 -0400

    Added Sample3_SecondClass and helloWorld method

--- php_sample3.h
+++ php_sample3.h
@@ -4,6 +4,10 @@
 #define PHP_SAMPLE3_EXTNAME "sample3"
 #define PHP_SAMPLE3_EXTVER	"1.0"
 
+#ifdef ZTS
+#include "TSRM.h"
+#endif
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -12,5 +16,7 @@
 
 extern zend_module_entry sample3_module_entry;
 #define phpext_sample3_ptr &sample3_module_entry
+
+PHP_MINIT_FUNCTION(sample3);
 
 #endif /* PHP_SAMPLE3_H */
--- sample3.c
+++ sample3.c
@@ -1,13 +1,8 @@
 #include "php_sample3.h"
 
-static function_entry php_sample3_functions[] = {
+static zend_function_entry php_sample3_functions[] = {
 	{ NULL, NULL, NULL }
 };
-
-PHP_MINIT_FUNCTION(sample3)
-{
-	return SUCCESS;
-}
 
 zend_module_entry sample3_module_entry = {
 	STANDARD_MODULE_HEADER,
@@ -22,6 +17,31 @@ zend_module_entry sample3_module_entry = {
 	STANDARD_MODULE_PROPERTIES
 };
 
+PHP_METHOD(Sample3_SecondClass, helloWorld)
+{
+	php_printf("Hello World\n");
+}
+
 #ifdef COMPILE_DL_SAMPLE3
 ZEND_GET_MODULE(sample3)
 #endif
+
+#define PHP_SAMPLE3_SC_NAME "Sample3_SecondClass"
+
+static const zend_function_entry php_sample3_sc_methods[] = {
+	PHP_ME(Sample3_SecondClass, helloWorld, NULL, ZEND_ACC_PUBLIC)
+	PHP_MALIAS(Sample3_SecondClass, sayHi, helloWorld, NULL, ZEND_ACC_PUBLIC)
+	{ NULL, NULL, NULL }
+};
+
+zend_class_entry *php_sample3_sc_entry;
+
+PHP_MINIT_FUNCTION(sample3)
+{
+	zend_class_entry ce;
+
+	INIT_CLASS_ENTRY(ce, PHP_SAMPLE3_SC_NAME, php_sample3_sc_methods);
+	php_sample3_sc_entry = zend_register_internal_class(&ce TSRMLS_CC);
+
+	return SUCCESS;
+}
